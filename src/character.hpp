24f3f8df7b4c40dd298d3322812b89614da75e3c
Sergei Avdoshkin <iocaster@Sergeis-MacBook-Air.local>
2024-04-18 15:53:57 +0800

    initial commit

---
+++ src/character.hpp
@@ -0,0 +1,14 @@
+class Character {
+public:
+    
+protected:
+    int move;
+    int weaponSkill;
+    int ballisticSkill;
+    int strength;
+    int toughness;
+    int wounds;
+    int attacks;
+    int leadership;
+    int save;
+};
