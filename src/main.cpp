4dc3b4e22e07925c96bb7c161764f3a2e88021fe
Sergei <delusional.drago@gmail.com>
2024-09-23 20:40:28 +0800

    cleaned main()

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,5 @@
 #include "datasheet.hpp"
 
 int main() {
-    std::cout << "Input character 1 params:" << std::endl;
-    auto character1 = std::unique_ptr<Datasheet>();
-    std::cout << "Input character 2 params:" << std::endl;
-    auto character2 = std::unique_ptr<Datasheet>();
     return 0;
 }
