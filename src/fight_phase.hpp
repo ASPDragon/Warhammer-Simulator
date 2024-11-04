13923e9d94fbb1a0f32e908378b73d0ed49a2ea3
Sergei <delusional.drago@gmail.com>
2024-11-04 23:44:30 +0800

    minor update

--- src/fight_phase.hpp
+++ src/fight_phase.hpp
@@ -1,10 +1,18 @@
 #include <vector>
 
 class Player;
+class Unit;
+class Model;
 
 class FightPhase {
+public:
     FightPhase();
-    
+    void pileIn(const Unit& unit);
+    size_t calculateAttacks(const Model& model) const;
+
+protected:
+    bool hasCharged(const Unit& unit) const;
+    bool canFight(const Model& model) const;
 private:
     std::vector<Player> players;
 };
